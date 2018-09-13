46a9883e05f6834ed210f8a5f4fe5b33809fdc58
Josh <josh@Creachaanganayi.lan>
2018-09-13 12:18:20 +0200

    Setup

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { doIntenseTask } from './utils/tasks';
 
 if (environment.production) {
   enableProdMode();
 }
-
-// doIntenseTask();
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
--- src/utils/tasks.ts
+++
@@ -1,6 +0,0 @@
-export function doIntenseTask() {
-  const now = new Date().getTime();
-  while (new Date().getTime() - now < 6000) {
-    // doing something intense
-  }
-}
